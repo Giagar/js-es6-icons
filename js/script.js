d7e6df268df34e726108d33f3a886634d1542c37
Gianpaolo <pao.barga@gmail.com>
2021-01-21 16:42:18 +0100

    milestone 2: fatta (da fare: convertire for annidato in es6)

--- js/script.js
+++ js/script.js
@@ -40,4 +40,36 @@ iconsList.forEach(el => {
     iconsContainer.innerHTML += icon;
 });
 
+// MILESTONE 2
+const colorsList = [
+    { animal: "blue" },
+    { vegetable: "orange" },
+    { people: "purple" },
+]
 
+// riprova in es6
+let iconsWithColorsList = [];
+
+for(let i = 0; i < iconsList.length; i++) {
+    for(let l = 0; l < colorsList.length; l++) {
+        console.log(iconsList[i].type, colorsList[l][iconsList[i].type])
+        if(iconsList[i].type, colorsList[l][iconsList[i].type]) {
+            iconsWithColorsList.push({ ...iconsList[i], color: colorsList[l][iconsList[i].type] })
+        }
+    }
+}
+// /riprova in es6
+
+iconsContainer.innerHTML = "";
+
+iconsWithColorsList.forEach(el => {
+    const {name, prefix, family, color} = el;
+
+    let icon = `
+        <div class="icon-container">
+            <i class="${family} ${prefix}${name}" style="color: ${color};"></i> <br/>
+            <span>${name}</span>
+        </div>`
+    
+    iconsContainer.innerHTML += icon;
+});
